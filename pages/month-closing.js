6fca49b6afd7fcce289325c65a29b06586ea0ba9
Luanda Rezende <luanda.rezende@hotmail.com>
2022-05-28 22:01:00 -0300

    fix ajusts and page month closing

--- pages/month-closing.js
+++ pages/month-closing.js
@@ -1,4 +1,4 @@
-import { Navbar, Nav, Container, NavDropdown, Navigation, NavItem, MenuItem, Button,Offcanvas, Table } from "react-bootstrap";
+import { Navbar, Nav, Container, NavDropdown, OverlayTrigger, Popover, Navigation, NavItem, MenuItem, Button,Offcanvas, Table } from "react-bootstrap";
 import Link from "next/link";
 import Image from "next/image";
 import { useContext } from "react";
@@ -15,6 +15,8 @@ import { BsGraphUp } from "react-icons/bs";
 import { SiContactlesspayment } from "react-icons/si";
 import { GiCoffeeCup } from "react-icons/gi";
 
+import { FcInfo } from "react-icons/fc";
+
 import FilterCalendarAndSeller from "../components/FilterCalendarAndSeller";
 
 import Form from 'react-bootstrap/Form';
@@ -24,124 +26,50 @@ import NavbarPanel from "../components/NavbarPanel";
 
 export default function Travel() {
   const [show, setShow] = useState(false);
+  const [dataTable, setDataTable] = useState(
+    [
+      { id: 1, date: "05/03/2022", paidVoucher: 320, cashSales: 220, total: 540, kg: '200', travelExpenses: 200, totalCommission: 0  },
+      { id: 2, date: "06/03/2022", paidVoucher: 322, cashSales: 150, total: 472, kg: '150', travelExpenses: 100, totalCommission: 0 },
+      { id: 3, date: "07/03/2022", paidVoucher: 280, cashSales: 410, total: 690.52, kg: '800', travelExpenses: 50, totalCommission: 0  },
+    ]
+  );
+  const [totalComission, setTotalComission] = useState(0);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  function calcComission(porcentage, id){
+    const _tempData = [...dataTable];
 
-  const options = {
-    chart: {
-      type: 'column'
-    },
-    title: '',
-     xAxis: {
-        categories: [
-            'Março',
-            'Abril',
-            'Maio',
-        ],
-        crosshair: true
-    },
-    yAxis: {
-        min: 0,
-        title: {
-            text: 'Quantidade'
-        }
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    plotOptions: {
-        column: {
-            pointPadding: 0.2,
-            borderWidth: 0
-        },
-        dataLabels: {
-          enabled: false           
-       },
-       showInLegend: true
-    },
-    legend: {
-      align: 'center',
-      verticalAlign: 'top',
-      layout: 'horizontal',
-      x: 0,
-      y: 0,
-    },
-    series: [{
-        name: 'Quantidade de clientes',
-        data: [49.9, 71.5, 106.4],
-        color: '#acd6fa',
-    }, {
-        name: 'Quantidade de vendas',
-        data: [83.6, 78.8, 98.5],
-        color: '#fadf98'
-    }, {
-        name: 'Quantidade de gastos',
-        data: [48.9, 38.8, 39.3],
-        color: '#faa4aa'
-    }]
-  };
+    if(porcentage === "1"){
+      _tempData[id].totalCommission = (_tempData[id].total * (( 1 / 100))).toFixed(2);
+    }else if(porcentage === "1.5"){
+      _tempData[id].totalCommission = (_tempData[id].total * (( 1.5 / 100))).toFixed(2);
+    }else if(porcentage === "3"){
+      _tempData[id].totalCommission = (_tempData[id].total * (( 3 / 100))).toFixed(2);
+    }else{
+      _tempData[id].totalCommission = 0;
+    }
 
-  const optionsPie = {
-    chart: {
-      type: 'pie'
-  },
-  title: {
-      text: 'Quantidade de vendas'
-  },
-  subtitle:{
-    text: '(Últimos 6 meses)'
-  },
-  tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  },
-  plotOptions: {
-      pie: {
-          cursor: 'pointer',
-          dataLabels: {
-              enabled: false
-          },
-          showInLegend: true
-      }
-  },
-  series: [{
-      name: 'Quantidade de vendas',
-      colorByPoint: true,
-      data: [{
-          name: 'Dezembro',
-          y: 81.40,
-          color: '#f7464a',
-      },{
-          name: 'Janeiro',
-          y: 61.41,
-          color: '#46bfbd',
-      }, {
-          name: 'Fevereiro',
-          y: 11.84,
-          color: '#ac64ad',
-      }, {
-          name: 'Março',
-          y: 10.85,
-          color: '#4d5360',
-      },{
-          name: 'Abril',
-          y: 25.50,
-          color: '#949fb1',
-      },
-      {
-          name: 'Maio',
-          y: 11.10,
-          color: '#e5e5e5',
-      }]
-  }]
-  };
+    calcTotalComission();
 
-  const src = `http://cafeminaspuro.com.br/wp-content/uploads/2020/01/cropped-logoMinasCafe-2-140x83.png`;
+    setDataTable(_tempData);
+  }
+
+  function calcTotalComission(){
+    let total = 0;
+    for(let i=0; i < dataTable.length; i++){
+      total = total + Number(dataTable[i].totalCommission);
+    }
+
+    setTotalComission(total.toFixed(2))
+  }
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Header as="h3">Informativo</Popover.Header>
+      <Popover.Body>
+         A comissão é calculada pelo total de vendas * ( porcentagem / 100);
+      </Popover.Body>
+    </Popover>
+  );
 
   return (
     <div style={{display: 'flex'}}>
@@ -157,7 +85,12 @@ export default function Travel() {
        <FilterCalendarAndSeller></FilterCalendarAndSeller>
 
       <div style={{background: '#fff', margin: '30px', padding: '25px'}}>
-       <p style={{color: '#495057'}}><FaMoneyCheckAlt style={{fontSize:'20px'}}></FaMoneyCheckAlt><span style={{marginLeft:'5px'}}>FECHAMENTO DO MÊS</span></p>
+       <p style={{color: '#495057'}}><FaMoneyCheckAlt style={{fontSize:'20px'}}></FaMoneyCheckAlt>
+        <span style={{marginLeft:'5px'}}>FECHAMENTO DO MÊS</span>
+        <OverlayTrigger placement="right" overlay={popover}>
+            <button style={{background:'#fff', border:'none', cursor: 'default'}}><FcInfo></FcInfo></button>
+        </OverlayTrigger>
+       </p>
       <Table responsive striped bordered hover>
         <thead>
           <tr>
@@ -173,64 +106,38 @@ export default function Travel() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>05/03/2022</td>
-            <td>R$320,00</td>
-            <td>R$360,00</td>
-            <td>R$50,00</td>
-            <td>R$100,00</td>
-            <td>R$160,00</td>
-            <td>
-            <Form.Select aria-label="Default select example">
-              <option>-</option>
-              <option value="1">1%</option>
-              <option value="1.5">1.5%</option>
-              <option value="3">3%</option>
-            </Form.Select>
-            </td>
-            <td>R$500</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>06/03/2022</td>
-            <td>R$322,00</td>
-            <td>R$80,00</td>
-            <td>R$110,00</td>
-            <td>R$100,00</td>
-            <td>R$60,00</td>
-            <td>
-            <Form.Select aria-label="Default select example">
-              <option>-</option>
-              <option value="1">1%</option>
-              <option value="1.5">1.5%</option>
-              <option value="3">3%</option>
-            </Form.Select>
-            </td>
-            <td>R$800</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>07/03/2022</td>
-            <td>R$280,00</td>
-            <td>R$35,00</td>
-            <td>R$100,00</td>
-            <td>R$200,00</td>
-            <td>R$60,00</td>
-            <td>
-            <Form.Select aria-label="Default select example">
-              <option>-</option>
-              <option value="1">1%</option>
-              <option value="1.5">1.5%</option>
-              <option value="3">3%</option>
-            </Form.Select>
-            </td>
-            <td>R$250</td>
-          </tr>         
+          {
+            dataTable.map((value, key) => {
+              return (
+                <tr key={key}>
+                  <td>{value.id}</td>
+                  <td>{value.date}</td>
+                  <td>R${value.paidVoucher}</td>
+                  <td>R${value.cashSales}</td>
+                  <td>R${value.total}</td>
+                  <td>{value.kg}</td>
+                  <td>R${value.travelExpenses}</td>
+                  <td>
+                    <Form.Select onChange={(e) => calcComission(e.target.value, key)}>
+                      <option>-</option>
+                      <option value="1">1%</option>
+                      <option value="1.5">1.5%</option>
+                      <option value="3">3%</option>
+                     </Form.Select>
+                  </td>
+                  <td>R${value.totalCommission}</td>
+                </tr>
+              )
+            })
+          }
         </tbody>
     </Table> 
 
-    <p>Total de comissão: R$1550 </p>
+    <p>Total de comissão: R$ {totalComission} </p>
+         <Button style={{float:'right', marginTop: '-33px'}} variant="primary">
+            Salvar comissão
+         </Button>
+
 
       </div>
 
